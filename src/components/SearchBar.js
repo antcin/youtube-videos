b7113b2239c2548f8d03033011dac145efe6ae3b
antcin <ciniglioantonio@gmail.com>
2018-05-30 12:00:07 +0100

    update state of SearchBar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,9 +4,19 @@ import React, { Component } from 'react';
 //define a new class called SearchBar and give it all the functionalities from React.Component
 // because we want SearchBar to be able to communicate with other components, we 'promoted' it from Functional to Class-based component
 class SearchBar extends Component {
+  //initialise state
+  constructor(props) {
+    super(props); // call the parent method
+
+    this.state = { term: '' }; // state is initialised by creating a new object and assigning it to this.state. The object we pass contains properties that we want to record in state. In this case we want to record the property `term` on state. Whenever user updates the search input, `term ` is the property we want to record that change on.
+  }
   //every class-based React component needs a render method
   render() {
-    return <input onChange={event => console.log(event.target.value) } />
+    return (
+      <div>
+        <input onChange={ event => this.setState({ term: event.target.value })} />
+      </div>
+    );
   }
 }
 
