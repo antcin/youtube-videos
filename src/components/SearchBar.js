143a3a13eba02bba8a36bf5ee9253f9a9afcff4d
antcin <ciniglioantonio@gmail.com>
2018-05-30 11:17:26 +0100

    promote SearchBar from functional to class-based component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 // define the component
-const SearchBar = () => {
-  return <input />;
+//define a new class called SearchBar and give it all the functionalities from React.Component
+// because we want SearchBar to be able to communicate with other components, we 'promoted' it from Functional to Class-based component
+class SearchBar extends Component {
+  //every class-based React component needs a render method
+  render() {
+    return <input />
+  }
 }
 
 export default SearchBar;
