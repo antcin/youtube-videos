c0821bb4e9ab92906274402d58637e09f1512954
antcin <ciniglioantonio@gmail.com>
2018-05-30 11:03:00 +0100

    import youtube api

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react'; //used to create react components
 import ReactDOM from 'react-dom'; //used to interact with DOM
 
+const API_KEY = 'AIzaSyAI9gv4_HoVdJ293SsHdZ3R7ouGRBfuGTI';
+
 // Create new component. this component should produce some HTML
 const App = () => {
   return <div>Hi!</div>;
