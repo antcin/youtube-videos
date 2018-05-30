c9fffc748d0f6334cfe70d8dba47fe63fc6f4489
antcin <ciniglioantonio@gmail.com>
2018-05-30 13:26:10 +0100

    promote App from functional to class component

--- src/index.js
+++ src/index.js
@@ -1,22 +1,30 @@
-import React from 'react'; //used to create react components
+import React, { Component } from 'react'; //used to create react components
 import ReactDOM from 'react-dom'; //used to interact with DOM
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/SearchBar';
-
 const API_KEY = 'AIzaSyAI9gv4_HoVdJ293SsHdZ3R7ouGRBfuGTI';
 
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
-  console.log(data);
-});
-
 // Create new component. this component should produce some HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
+
 
 // Take this component's generated HTML and put it on the page(in the DOM)
 ReactDOM.render(<App />, document.querySelector('.container'));
