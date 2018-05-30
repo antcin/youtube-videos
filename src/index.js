2b812d8fcb32f2daa99510c6d0a07ceb9ece1c32
antcin <ciniglioantonio@gmail.com>
2018-05-30 14:36:35 +0100

    can search for new content

--- src/index.js
+++ src/index.js
@@ -17,7 +17,11 @@ class App extends Component {
       selectedVideo: null
     };
 
-    YTSearch({key: API_KEY, term: 'react.js'}, (videos) => {
+    this.videoSearch('react.js');
+  }
+
+  videoSearch(term) {
+    YTSearch({key: API_KEY, term: term}, (videos) => {
       this.setState({
         videos: videos,
         selectedVideo: videos[0]
@@ -26,12 +30,10 @@ class App extends Component {
     });
   }
 
-
-
   render() {
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)}/>
         <VideoDetail video={this.state.selectedVideo}/>
         <VideoList
           onVideoSelect={selectedVideo => this.setState({selectedVideo})}
